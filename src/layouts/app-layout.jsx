e6bfcfb87854c2e4784da3507682b8cd9f742a34
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-15 15:06:31 +0530

    added layout

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+function AppLayout() {
+  return (
+    <div>
+      <main className="container min-h-screen mx-auto">
+        <Header />
+        <Outlet />
+      </main>
+      {/* <Footer /> */}
+      <div className="p-10 mt-10 text-center text-gray-800">
+        Made by Praveen Raj
+      </div>
+    </div>
+  );
+}
+
+export default AppLayout;
